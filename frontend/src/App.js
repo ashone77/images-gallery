ce20c8c820f1902f4b087142ee538360c21a522e
Aswin Haridas <aswinvharidas@gmail.com>
2022-07-07 14:48:31 +0530

    Header component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title="Images Gallery"></Header>
     </div>
   );
 }
