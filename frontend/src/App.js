8f27411e8700848eca4f9fea253c6df73de4824f
Aswin Haridas <aswinvharidas@gmail.com>
2022-07-08 14:04:24 +0530

    added api keys and api fetch success

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
 import Search from './components/search';
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 function App() {
 
@@ -13,9 +13,16 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) =>{
+        console.log(data);
+      })
+      .catch((err) =>{
+          console.log(err);
+      });
   }
 
-  console.log(word)
 
   return (
     <div className="App">
